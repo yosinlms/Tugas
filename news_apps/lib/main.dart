b6e15ee1052766f13895796c76a0d2f5c71c8215
yosinlms <yosiputra29@gmail.com>
2025-12-13 21:34:41 +0700

    store

--- news_apps/lib/main.dart
+++ news_apps/lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:news_apps/onBoarding.dart';
+import 'package:http/http.dart' as http;
+import 'package:news_apps/Model/product.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +12,141 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      // halaman pertama yang ditampilkan
-      home: const Onboarding(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  Future<List<Product>> fetchProducts() async {
+    final url = Uri.parse('https://fakestoreapi.com/products');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final List data = jsonDecode(response.body);
+      return data.map((e) => Product.fromJson(e)).toList();
+    } else {
+      throw Exception('Failed to load products');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+            'Products Grid',
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.w500),
+          ),
+          backgroundColor: const Color.fromARGB(255, 20, 100, 165)),
+      body: FutureBuilder<List<Product>>(
+        future: fetchProducts(),
+        builder: (context, snapshot) {
+          // ⏳ loading
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          // ❌ error
+          if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          }
+
+          // 📦 data kosong
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(
+              child: Text(
+                'No products found',
+                style: TextStyle(color: Colors.white),
+              ),
+            );
+          }
+
+          // ✅ DATA SIAP
+          final products = snapshot.data!;
+
+          return GridView.builder(
+            padding: const EdgeInsets.all(12),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 2,
+              mainAxisSpacing: 2,
+              childAspectRatio: 0.7,
+            ),
+            itemCount: products.length,
+            itemBuilder: (context, index) {
+              final product = products[index];
+
+              return Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      // 🖼 Image (TOP)
+                      Expanded(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            product.image,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+
+                      // 📝 Title
+                      Text(
+                        textAlign: TextAlign.center,
+                        product.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      SizedBox(height: 6),
+
+                      // 💲 Price
+                      Text(
+                        '\$${product.price}',
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 66, 152, 69),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      SizedBox(
+                          width: double.infinity,
+                          height: 40,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor:
+                                      const Color.fromARGB(255, 20, 100, 165),
+                                  foregroundColor: Colors.white),
+                              onPressed: () {},
+                              child: Text('Buy')))
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
